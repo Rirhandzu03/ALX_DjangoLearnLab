7e9b2347d43190bd82881235f72d50462a757804
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-13 22:34:15 +0200

    Advanced Django Models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,23 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 
+
+# Admin view, only accessible by users with the 'Admin' role
+@user_passes_test(lambda u: u.userprofile.role == 'Admin')
+def admin_view(request):
+    return render(request, 'admin_page.html')  # Admin page template
+
+# Librarian view, only accessible by users with the 'Librarian' role
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+def librarian_view(request):
+    return render(request, 'librarian_page.html')  # Librarian page template
+
+# Member view, only accessible by users with the 'Member' role
+@user_passes_test(lambda u: u.userprofile.role == 'Member')
+def member_view(request):
+    return render(request, 'member_page.html')  # Member page template
 
 # Function-based view
 def list_books(request):
