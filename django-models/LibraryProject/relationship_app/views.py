483ae0f0bf35d5168405318544549b54563d7187
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-13 18:55:29 +0200

    Advanced Django Projects

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
-from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+
 
 # Function-based view
 def list_books(request):
