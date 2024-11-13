262ba704f55938852fcea9d0c941673a136d3f81
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-13 04:22:33 +0200

    Advanced django projects

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import LibraryDetailView, list_books
 
 urlpatterns = [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Author
 from django.views.generic.detail import DetailView
 from .models import Library
 
