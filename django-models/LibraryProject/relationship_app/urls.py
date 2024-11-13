4f31c98f83267586f3361e7960d91e559a003cff
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-13 22:22:59 +0200

    Advanced Django Models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     path('register/', views.register, name='register'), 
 
     # Configuration URLS
-    path('admin/', admin_view, name='admin_view'),
-    path('librarian/', librarian_view, name='librarian_view'),
-    path('member/', member_view, name='member_view'),
+    path('admin/', views.admin_view, name='admin_view'), 
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/',views.member_view, name='member_view'),
 ]
 
