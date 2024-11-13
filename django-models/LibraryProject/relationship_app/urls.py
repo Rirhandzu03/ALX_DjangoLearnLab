0016f6cc0067471e9290a5aca5e5a89d136733fa
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-13 18:49:06 +0200

    Advanced Django Projects

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 from .views import list_books, LibraryDetailView 
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
     path('books/', list_books, name='list_books'),  # Function-based view for listing books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for library detail
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from .models import Book, Library
 from django.views.generic.detail import DetailView
-from .models import Library
 
 # Function-based view
 def list_books(request):
@@ -14,4 +16,37 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form .is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Registration failed. Please try again.')
 
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form}) 
+    
+
+# Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, 'Invalid credentials. Please try again.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+# Logout View
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
