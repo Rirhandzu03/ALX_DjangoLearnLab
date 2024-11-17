16ed3d1f6ccce3a943e905040cc2fa6dd65443d0
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-11-18 00:51:00 +0200

    Advanced Django Projects

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -28,6 +28,28 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# Configuring Django for HTTPS Support
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 31536000
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
+
+# Securing Cookies
+SESSION_COOKIE_SECURE = True
+
+CSFR_COOKIE_SECURE = True
+
+# Implementing Secure Headers
+X_FRAME_OPTIONS = "DENY"
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
 # Enable the XSS filter in the browser(secure_setting)
 SECURE_BROWSER_XSS_FILTER = True
 
