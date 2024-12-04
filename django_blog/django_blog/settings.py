1add5603594c1f1271493205748480fafc9c3180
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-12-05 00:20:53 +0200

    modified settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,6 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': '#Lerato0903',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
