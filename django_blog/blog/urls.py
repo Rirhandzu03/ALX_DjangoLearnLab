0a6fc0a39b5bf8a49127c9a604cd687949c36399
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-12-05 16:26:46 +0200

    Modified views.py in blog app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import register, CustomLoginView, CustomLogoutView
+from .views import register, CustomLoginView, CustomLogoutView, profile
 
 urlpatterns = [  # Corrected here
+
     path('register/', register, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('profile', views.profile, name='profile'),
 ]
