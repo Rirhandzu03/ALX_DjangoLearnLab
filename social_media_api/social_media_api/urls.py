f9b93c0dce34443de0b5009fd6e7533bcd94fe2e
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-12-10 23:56:07 +0200

    modified urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', home, name='home'),
-    path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
 ]
