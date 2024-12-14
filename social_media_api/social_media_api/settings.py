f9e9a2823d22233641c8193f4908807a7355b138
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-12-14 21:38:55 +0200

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2*l$^#)2w4fj&+k4in^pi(tpl)r9z9q+^zde2$$=*4chj7s*=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
