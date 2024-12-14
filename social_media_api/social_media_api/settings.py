459b5a046cf1ae1d9d4cac133a7552192fd49ea4
Rirhandzu Shilowa <rirhandzuhillary@gmail.com>
2024-12-14 21:53:00 +0200

    modified settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +51,9 @@ INSTALLED_APPS = [
     'accounts',
     'posts',
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Custom user model
 AUTH_USER_MODEL = 'accounts.CustomUser'
